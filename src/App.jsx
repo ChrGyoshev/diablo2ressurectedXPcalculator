f4a0c2acf55bb16721420684d6ee2c425652f701
ChrGyoshev <krischan@abv.bg>
2024-01-29 20:08:41 +0200

    added new font and some tweaks

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import "./App.css";
-
+import "./assets/fonts/diablo.ttf";
 import player from "./datasheet";
 
 function App() {
   return (
     <>
-      <div className="background d-flex justify-content-center">
-        <div className="wrapper container-fluid ">
-          <h1>testing</h1>
-        </div>
+      <div className="background d-flex justify-content-center align-content-center">
+        <span className="wrapper container-fluid d-flex justify-content-center m-4 p-5 rounded">
+          <h1 className="chrisey">Diablo II XP Calculator</h1>
+        </span>
       </div>
     </>
   );
