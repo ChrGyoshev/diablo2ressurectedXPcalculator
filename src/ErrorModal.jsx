505fcee0dd94cfb3ed69d64ff42cecf8f4150643
ChrGyoshev <krischan@abv.bg>
2024-03-02 16:26:16 +0200

    header edited

--- src/ErrorModal.jsx
+++ src/ErrorModal.jsx
@@ -1,5 +1,36 @@
-const ErrorBox = () => {
-  return <p>Error</p>;
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+const ErrorBox = ({ errorHandler }) => {
+  return (
+    <Modal
+      show={true} // Ensure the modal is shown
+      onHide={errorHandler} // Close modal when clicking outside
+      className="w-90 opacity-100"
+      centered
+    >
+      <Modal.Header closeButton onClick={errorHandler}>
+        <p className="flex-grow-1 m-0 text">ERROR</p>
+      </Modal.Header>
+
+      <Modal.Body>
+        <h1 className="m-5 text-center">
+          <span className="text-danger">Something went wrong! Try Again!</span>
+        </h1>
+      </Modal.Body>
+
+      <Modal.Footer>
+        <div className="d-flex justify-content-center w-100">
+          <Button
+            variant="primary"
+            className="ps-5 pe-5"
+            onClick={errorHandler}
+          >
+            Close
+          </Button>
+        </div>
+      </Modal.Footer>
+    </Modal>
+  );
 };
 
 export default ErrorBox;
--- src/Result.jsx
+++ src/Result.jsx
@@ -61,36 +61,7 @@ function Result({ clickHandler, formData, resetHandler }) {
           </Modal.Footer>
         </Modal>
       ) : (
-        <Modal
-          show={true} // Ensure the modal is shown
-          onHide={errorHandler} // Close modal when clicking outside
-          className="w-90 opacity-100"
-          centered
-        >
-          <Modal.Header closeButton onClick={handler}>
-            <p className="flex-grow-1 m-0 text">ERROR</p>
-          </Modal.Header>
-
-          <Modal.Body>
-            <h1 className="m-5 text-center">
-              <span className="text-danger">
-                Something went wrong! Try Again!
-              </span>
-            </h1>
-          </Modal.Body>
-
-          <Modal.Footer>
-            <div className="d-flex justify-content-center w-100">
-              <Button
-                variant="primary"
-                className="ps-5 pe-5"
-                onClick={errorHandler}
-              >
-                Close
-              </Button>
-            </div>
-          </Modal.Footer>
-        </Modal>
+        <ErrorBox errorHandler={errorHandler} />
       )}
     </div>
   );
